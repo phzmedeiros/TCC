df7c856bfbf936be2761f73d44290425cd9d1c3e
Pedro Henrique Medeiros dos Reis <81303691+phzmedeiros@users.noreply.github.com>
2023-11-15 22:46:57 -0300

    consulta de cpf
    atualizei a area dos voluntários e melhorei o design e o front de bobeira

--- ano/View/usuarios_cadastrados.php
+++ ano/View/usuarios_cadastrados.php
@@ -414,9 +414,55 @@ if (!isset($_SESSION["usuarioSenha"])) {
         </div>
         <div class="main-content">
           <div class="table">
+          <h2>Busca por CPF</h2>
+            <br>
+            <!-- Tabela de resultados da busca por CPF -->
+            <table>
+              <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Endereço</th>
+                <th>Profissão</th>
+                <th>Celular</th>
+                <th>Telefone de Emergencia</th>
+                <th>RG</th>
+                <th>CPF</th>
+                <th>Equipe que pertence</th>
+                <th>Observações</th>
+                <th>Opções</th>
+              </tr>
+              <?php 
+                // Verifica se um CPF de pesquisa foi submetido
+                if (isset($_POST['searchCpf'])) {
+                  $searchCpf = $_POST['searchCpf'];
+                  require_once '../Model/crud.php';
+                  $crud = new crud();
+                  $usuariosEncontrados = $crud->selecionar_Um_Usuario_Por_Cpf($searchCpf);
+
+                  // Exibe os resultados da busca
+                  foreach ($usuariosEncontrados as $usuario) {
+                    echo "<tr>";
+                    echo "<td>{$usuario['nome_do_voluntario']}</td>";
+                    echo "<td>{$usuario['email']}</td>";
+                    echo "<td>{$usuario['endereco']}</td>";
+                    echo "<td>{$usuario['profissao']}</td>";
+                    echo "<td>{$usuario['cell']}</td>";
+                    echo "<td>{$usuario['tell_emergencia']}</td>";
+                    echo "<td>{$usuario['rg']}</td>";
+                    echo "<td>{$usuario['cpf']}</td>";
+                    echo "<td>{$usuario['equipe_pertencente']}</td>";
+                    echo "<td>{$usuario['obs']}</td>";
+                    echo "<td><a href='excluir_usuario.php?cpf={$usuario['cpf']}'>Excluir</a></td>";
+                    echo "</tr>";
+                  }
+                }
+              ?>
+            </table>
+            <br>
             <br>
             <h2>Voluntários</h2>
             <!-- Adicione suas tabelas ou outros conteúdos aqui -->
+            <br>
             <table>
               <tr>
                 <th>Nome</th>
@@ -450,50 +496,6 @@ if (!isset($_SESSION["usuarioSenha"])) {
                     echo "<td>{$usuario['obs']}</td>";
                     echo "<td><a href='excluir_usuario.php?cpf={$usuario['cpf']}'>Excluir</a></td>";
                     echo "</tr>";
-                }
-              ?>
-            </table>
-            <br><br>
-            <h2>Busca por CPF</h2>
-            <br>
-            <!-- Tabela de resultados da busca por CPF -->
-            <table>
-              <tr>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Endereço</th>
-                <th>Profissão</th>
-                <th>Celular</th>
-                <th>Telefone de Emergencia</th>
-                <th>RG</th>
-                <th>CPF</th>
-                <th>Equipe que pertence</th>
-                <th>Observações</th>
-                <th>Opções</th>
-              </tr>
-              <?php 
-                // Verifica se um CPF de pesquisa foi submetido
-                if (isset($_POST['searchCpf'])) {
-                  $searchCpf = $_POST['searchCpf'];
-                  $crud = new crud();
-                  $usuariosEncontrados = $crud->selecionar_Um_Usuario_Por_Cpf($searchCpf);
-
-                  // Exibe os resultados da busca
-                  foreach ($usuariosEncontrados as $usuario) {
-                    echo "<tr>";
-                    echo "<td>{$usuario['nome_do_voluntario']}</td>";
-                    echo "<td>{$usuario['email']}</td>";
-                    echo "<td>{$usuario['endereco']}</td>";
-                    echo "<td>{$usuario['profissao']}</td>";
-                    echo "<td>{$usuario['cell']}</td>";
-                    echo "<td>{$usuario['tell_emergencia']}</td>";
-                    echo "<td>{$usuario['rg']}</td>";
-                    echo "<td>{$usuario['cpf']}</td>";
-                    echo "<td>{$usuario['equipe_pertencente']}</td>";
-                    echo "<td>{$usuario['obs']}</td>";
-                    echo "<td><a href='excluir_usuario.php?cpf={$usuario['cpf']}'>Excluir</a></td>";
-                    echo "</tr>";
-                  }
                 }
               ?>
             </table>
